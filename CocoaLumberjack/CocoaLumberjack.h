db068025097ca7179dfc3001ed20f24374207a98
Arlo O'Keeffe <MailToArlo@googlemail.com>
2012-10-03 13:03:33 -0600

    Added iOS Static Framework project

---
+++ CocoaLumberjack/CocoaLumberjack.h
@@ -0,0 +1,13 @@
+//
+//  CocoaLumberjack.h
+//  CocoaLumberjack
+//
+//  Created by Arlo Louis O'Keeffe on 2012-10-03.
+//  Copyright (c) 2012 ASE Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaLumberjack : NSObject
+
+@end
---
+++ CocoaLumberjack/CocoaLumberjack.m
@@ -0,0 +1,13 @@
+//
+//  CocoaLumberjack.m
+//  CocoaLumberjack
+//
+//  Created by Arlo Louis O'Keeffe on 2012-10-03.
+//  Copyright (c) 2012 ASE Group. All rights reserved.
+//
+
+#import "CocoaLumberjack.h"
+
+@implementation CocoaLumberjack
+
+@end
