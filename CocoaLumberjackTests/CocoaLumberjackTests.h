96b4f869a779eef527b81b1297ddb2df4190fd5b
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-21 18:42:35 -0600

    Added standard tests

---
+++ CocoaLumberjackTests/CocoaLumberjackTests.h
@@ -0,0 +1,5 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CocoaLumberjackTests : SenTestCase
+
+@end
---
+++ CocoaLumberjackTests/CocoaLumberjackTests.m
@@ -0,0 +1,24 @@
+#import "CocoaLumberjackTests.h"
+
+@implementation CocoaLumberjackTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CocoaLumberjackTests");
+}
+
+@end
